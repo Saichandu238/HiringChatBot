e8a3af9dad9481bf70e4ce89524e4a5011f9a5e2
PasunuriSaichandu <saichandupasunuri2002@gmail.com>
2025-05-27 21:02:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -197,9 +197,9 @@ elif st.session_state.stage == 'questions':
             st.markdown("====="*20)
 
     # End chat button: save score and show leaderboard
-    if st.button("🧹 Clear Leaderboard"):
-        clear_leaderboard()
-        st.session_state.clear_leaderboard = True
+    # if st.button("🧹 Clear Leaderboard"):
+    #     clear_leaderboard()
+    #     st.session_state.clear_leaderboard = True
         # st.success("Leaderboard cleared!")
     if st.button("End Chat", key="end_chat"):
         total_questions = len(st.session_state.answers)
